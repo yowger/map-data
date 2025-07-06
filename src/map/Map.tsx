3883c284a982d4803ddcb78b87681415578d90f7
Ver0018 <rogerpantilyowger@gmail.com>
2025-07-07 01:00:01 +0800

    added custom markers per category

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -18,6 +18,9 @@ import type {
 } from "../types/map"
 import type { MapReport } from "../types/reports"
 
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import { faFire } from "@fortawesome/free-solid-svg-icons"
+
 interface MapProps {
     clusters?: ClusterFeature[]
     selectedBarangay: BarangayFeature | null
@@ -151,6 +154,39 @@ function getLogPadding(zoom: number): number {
     return Math.min(1.2, Math.max(0.2, 1.5 - zoom * 0.04))
 }
 
+const markerStyles: Record<string, { icon: string; color: string }> = {
+    Flood: { icon: "fas fa-water", color: "#2563eb" },
+    Landslide: { icon: "fas fa-mountain", color: "#F54900" },
+    Garbage: { icon: "fas fa-trash", color: "#009966" },
+    "Road Damage": { icon: "fas fa-road", color: "#57534D" },
+    "Blocked Drainage": { icon: "fas fa-tint-slash", color: "#155DFC" },
+    "Power Outage": { icon: "fas fa-bolt", color: "#D08700" },
+    "Missing Person": { icon: "fas fa-user-slash", color: "#E7000B" },
+    "Missing Animal": { icon: "fas fa-dog", color: "#92400e" },
+    "Missing Vehicle": { icon: "fas fa-car-side", color: "#0084D1" },
+    "Missing Object": { icon: "fas fa-box-open", color: "#45556C" },
+    Other: { icon: "fas fa-question-circle", color: "#525252" },
+}
+
+function customMarker(iconClass: string, bgColor = "#3b82f6") {
+    return Leaflet.divIcon({
+        html: `
+        <div class="custom-marker">
+          <div class="icon-circle" style="--marker-color: ${bgColor};">
+            <i class="${iconClass} icon"></i>
+          </div>
+              <div class="marker-arrow" ></div>
+        </div>
+      `,
+        className: "",
+        iconSize: [40, 52],
+        iconAnchor: [15, 42],
+        popupAnchor: [0, -40],
+        shadowSize: [5, 5],
+        shadowAnchor: [10, 10],
+    })
+}
+
 export function ClusterLayer({ clusters, onClusterClick }: ClusterLayerProps) {
     const map = useMap()
     const bounds = map.getBounds()
@@ -174,17 +210,20 @@ export function ClusterLayer({ clusters, onClusterClick }: ClusterLayerProps) {
         const currentZoom = map.getZoom()
 
         setMarkerCache((prevMarkers) => {
+            let prevMarkersCopy = prevMarkers
+
             if (lastZoom !== currentZoom) {
                 setLastZoom(currentZoom)
 
-                return clusters
+                // flickers when zoomed, not big problem, fix later. for now reset markers to keep data fresh.
+                prevMarkersCopy = []
             }
 
             const prevIds = new Set(
-                prevMarkers.map((prevMarker) => getFeatureKey(prevMarker))
+                prevMarkersCopy.map((prevMarker) => getFeatureKey(prevMarker))
             )
 
-            const retainedClusters = prevMarkers.filter((prevMarker) => {
+            const retainedClusters = prevMarkersCopy.filter((prevMarker) => {
                 const [lng, lat] = prevMarker.geometry.coordinates
 
                 return paddedBounds.contains([lat, lng])
@@ -231,12 +270,18 @@ export function ClusterLayer({ clusters, onClusterClick }: ClusterLayerProps) {
                 } else {
                     const { id, type } = feature.properties
                     const markerId = `report-${id}`
+                    const markerStyle =
+                        markerStyles[type] || markerStyles["Other"]
 
                     return (
                         <Marker
                             key={markerId}
                             position={[lat, lng]}
                             opacity={1}
+                            icon={customMarker(
+                                markerStyle.icon,
+                                markerStyle.color
+                            )}
                         >
                             <Popup>
                                 <strong>Type:</strong> {type}
